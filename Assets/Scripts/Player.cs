bdb8410ce8dd1f9ca21b0d296a5d8ade034ec8e8
Purn <purnjay19@gmail.com>
2019-01-04 05:02:54 -0800

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+
+public class Player : MonoBehaviour {
+
+	//Floats
+	[Header("Player Speed")]
+	public float speed;
+
+	//Vectors
+	Vector2 direction;
+
+	//Components
+	Rigidbody2D rb2d;
+	Animator anim;
+
+	//Boolean
+	bool isWalking;
+
+	void Start () {
+	
+		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+
+	void Update () {
+
+		var h = Input.GetAxisRaw("Horizontal");
+		var v = Input.GetAxisRaw("Vertical");
+
+		direction = new Vector2(h, v);
+		rb2d.velocity = direction * speed;
+
+		isWalking = (Mathf.Abs (h) + Mathf.Abs (v)) > 0;
+
+		anim.SetBool ("isWalking", isWalking);
+
+		if (isWalking) {
+			anim.SetFloat ("X", h);
+			anim.SetFloat ("Y", v);
+
+
+		}
+	
+	}
+
+
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+	
+	public string sceneName;
+
+	public void LoadScene()
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+
+}
